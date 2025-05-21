729530b97422938b9b1755c2f5aae6f633cd3703
savage276 <jiangyao391@gmail.com>
2025-05-21 15:29:31 +0800

    Updated index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,7 +4,7 @@ import { createBrowserRouter} from "react-router-dom";
 
 const router = createBrowserRouter([
     {
-        path: "/login",
+        path: "/",
         element: <Login />
     },
     {
